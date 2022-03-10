13da1f64b2a225da184d5063718176646a342dce
Ahad Birang <farnabaz@gmail.com>
2022-03-10 08:07:45 +0000

    feat: configure nuxt3/vite hmr host in Gitpod env

--- .bash_aliases
+++ .bash_aliases
@@ -1,4 +1,3 @@
-
 # Git Aliases
 
 alias g='git'
---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Update Vite's HMR config in Nuxt 3
+if [[ "${GITPOD_WORKSPACE_URL}" ]]; then
+
+VITE_HMR_PORT=${VITE_HMR_PORT:-24678}
+cat > outfile.txt <<EOF
+vite.server.hmr.host=${GITPOD_WORKSPACE_URL/https:\/\//$VITE_HMR_PORT-}
+vite.server.hmr.clientPort=443
+vite.server.hmr.protocol=wss
+EOF
+
+fi;
