28133d278407262b583292cbc65d93d8ebd87032
Ahad Birang <farnabaz@gmail.com>
2022-03-10 11:25:27 +0000

    fix: nuxtrc path

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 if [[ "${GITPOD_WORKSPACE_URL}" ]]; then
 
 VITE_HMR_PORT=${VITE_HMR_PORT:-24678}
-cat > outfile.txt <<EOF
+cat > /home/gitpod/.nuxtrc <<EOF
 vite.server.hmr.host=${GITPOD_WORKSPACE_URL/https:\/\//$VITE_HMR_PORT-}
 vite.server.hmr.clientPort=443
 vite.server.hmr.protocol=wss
