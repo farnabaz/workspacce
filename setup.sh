508d35881a10e7e2035b685caf8c01cb5b3a305b
Ahad Birang <farnabaz@gmail.com>
2022-05-18 22:42:33 +0000

    feat: install ohmyzsh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-# Update Vite's HMR config in Nuxt 3
+
 if [[ "${GITPOD_WORKSPACE_URL}" ]]; then
 
+# Update Vite's HMR config in Nuxt 3
 VITE_HMR_PORT=${VITE_HMR_PORT:-24678}
 cat > /home/gitpod/.nuxtrc <<EOF
-vite.server.hmr.host=${GITPOD_WORKSPACE_URL/https:\/\//$VITE_HMR_PORT-}
-vite.server.hmr.clientPort=443
-vite.server.hmr.protocol=wss
+#vite.server.hmr.host=${GITPOD_WORKSPACE_URL/https:\/\//$VITE_HMR_PORT-}
+#vite.server.hmr.clientPort=443
+#vite.server.hmr.protocol=wss
+telemetry.enabled=false
 EOF
+
+# Install ohmyzsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 fi;
 
